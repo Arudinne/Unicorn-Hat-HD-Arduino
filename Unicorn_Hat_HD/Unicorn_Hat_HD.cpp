f090182086e0b9b6bc5aaa5490f9b50b5730fa55
Arudinne <brandoncallen@gmail.com>
2017-12-29 21:27:58 -0600

    Update Unicorn_Hat_HD.cpp
    Implemented some of the changes recommended by https://www.reddit.com/user/McChubby007, Still working on the last one.
    
    URL: https://www.reddit.com/r/arduino/comments/7kjm1s/im_still_a_novice_programmer_but_i_wrote_my_first/drexk7u/

--- Unicorn_Hat_HD/Unicorn_Hat_HD.cpp
+++ Unicorn_Hat_HD/Unicorn_Hat_HD.cpp
@@ -32,249 +32,253 @@ void Unicorn_Hat_HD::show(void)
 
 void Unicorn_Hat_HD::clear()
 {
-  for (int i=0; i <= 15; i++){
-    for (int j=0; j <= 15; j++){
-    _buff[i][j][0] = 0;
-    _buff[i][j][1] = 0;
-    _buff[i][j][2] = 0;
-    }
-  }
+	memset(_buff, 0, sizeof _buff);
 }
 
 void Unicorn_Hat_HD::setPixelColor(uint8_t n, uint8_t r, uint8_t g, uint8_t b)
 {
   uint8_t y;
   uint8_t x;
-  if(n >= 0 && n <= 15){
-    y = 0;
-    x = 0;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
+  
+  switch(n)
+  {
+	  case 0 ... 15:
+		  y = 0;
+		  x = 0;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 16 ... 31:
+		  y = 16;
+		  x = 1;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 32 ... 47:
+		  y = 32;
+		  x = 2;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 48 ... 63:
+		  y = 48;
+		  x = 3;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 64 ... 79:
+		  y = 64;
+		  x = 4;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 80 ... 95:
+		  y = 80;
+		  x = 5;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 96 ... 111:
+		  y = 96;
+		  x = 6;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 112 ... 127:
+		  y = 112;
+		  x = 7;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 128 ... 143:
+		  y = 128;
+		  x = 8;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 144 ... 159:
+		  y = 144;
+		  x = 9;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 160 ... 175:
+		  y = 160;
+		  x = 10;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 176 ... 191:
+		  y = 176;
+		  x = 11;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 192 ... 207:
+		  y = 192;
+		  x = 12;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 208 ... 223:
+		  y = 208;
+		  x = 13;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 224 ... 239:
+		  y = 224;
+		  x = 14;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 240 ... 255:
+		  y = 240;
+		  x = 15;
+		  _buff[(n-y)][x][0] = r;
+		  _buff[(n-y)][x][1] = g;
+		  _buff[(n-y)][x][2] = b;
+		  break;
+	  case 256:
+		break;
   }
-  else if(n >= 16 && n <= 31){
-    y = 16;
-    x = 1;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 32 && n <= 47){
-    y = 32;
-    x = 2;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 48 && n <= 63){
-    y = 48;
-    x = 3;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 64 && n <= 79){
-    y = 64;
-    x = 4;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 80 && n <= 95){
-    y = 80;
-    x = 5;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 96 && n <= 111){
-    y = 96;
-    x = 6;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 112 && n <= 127){
-    y = 112;
-    x = 7;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 128 && n <= 143){
-    y = 128;
-    x = 8;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 144 && n <= 159){
-    y = 144;
-    x = 9;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 160 && n <= 175){
-    y = 160;
-    x = 10;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 176 && n <= 191){
-    y = 176;
-    x = 11;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 192 && n <= 207){
-    y = 192;
-    x = 12;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 208 && n <= 223){
-    y = 208;
-    x = 13;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 224 && n <= 239){
-    y = 224;
-    x = 14;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 240 && n <= 255){
-    y = 240;
-    x = 15;
-    _buff[(n-y)][x][0] = r;
-    _buff[(n-y)][x][1] = g;
-    _buff[(n-y)][x][2] = b;
-    }
-  else if(n >= 256){} 
 }
 
 void Unicorn_Hat_HD::setPixelColor(uint8_t n, uint32_t c)
 {
   uint8_t y;
   uint8_t x;
-  if(n >= 0 && n <= 15){
-    y = 0;
-    x = 0;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
+  
+  switch(n)
+  {
+	  case 0 ... 15:
+		  y = 0;
+		  x = 0;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 16 ... 31:
+		  y = 16;
+		  x = 1;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 32 ... 47:
+		  y = 32;
+		  x = 2;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 48 ... 63:
+		  y = 48;
+		  x = 3;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 64 ... 79:
+		  y = 64;
+		  x = 4;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 80 ... 95:
+		  y = 80;
+		  x = 5;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 96 ... 111:
+		  y = 96;
+		  x = 6;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 112 ... 127:
+		  y = 112;
+		  x = 7;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 128 ... 143:
+		  y = 128;
+		  x = 8;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 144 ... 159:
+		  y = 144;
+		  x = 9;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 160 ... 175:
+		  y = 160;
+		  x = 10;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 176 ... 191:
+		  y = 176;
+		  x = 11;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 192 ... 207:
+		  y = 192;
+		  x = 12;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 208 ... 223:
+		  y = 208;
+		  x = 13;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 224 ... 239:
+		  y = 224;
+		  x = 14;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 240 ... 255:
+		  y = 240;
+		  x = 15;
+		  _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
+		  _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
+		  _buff[(n-y)][x][2] = (uint8_t)c;
+		  break;
+	  case 256:
+		break;
   }
-  else if(n >= 16 && n <= 31){
-    y = 16;
-    x = 1;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 32 && n <= 47){
-    y = 32;
-    x = 2;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 48 && n <= 63){
-    y = 48;
-    x = 3;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 64 && n <= 79){
-    y = 64;
-    x = 4;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 80 && n <= 95){
-    y = 80;
-    x = 5;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 96 && n <= 111){
-    y = 96;
-    x = 6;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 112 && n <= 127){
-    y = 112;
-    x = 7;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 128 && n <= 143){
-    y = 128;
-    x = 8;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 144 && n <= 159){
-    y = 144;
-    x = 9;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 160 && n <= 175){
-    y = 160;
-    x = 10;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 176 && n <= 191){
-    y = 176;
-    x = 11;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 192 && n <= 207){
-    y = 192;
-    x = 12;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 208 && n <= 223){
-    y = 208;
-    x = 13;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 224 && n <= 239){
-    y = 224;
-    x = 14;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 240 && n <= 255){
-    y = 240;
-    x = 15;
-    _buff[(n-y)][x][0] = (uint8_t)(c >> 16);
-    _buff[(n-y)][x][1] = (uint8_t)(c >> 8);
-    _buff[(n-y)][x][2] = (uint8_t)c;
-    }
-  else if(n >= 256){} 
 }
