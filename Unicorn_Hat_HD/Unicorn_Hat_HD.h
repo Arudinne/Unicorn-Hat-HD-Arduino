021229d70a7c7008a6be7385fdc31d0d300faf75
Arudinne <brandoncallen@gmail.com>
2017-12-30 02:28:29 -0600

    Update Unicorn_Hat_HD.h

--- Unicorn_Hat_HD/Unicorn_Hat_HD.h
+++ Unicorn_Hat_HD/Unicorn_Hat_HD.h
@@ -7,13 +7,16 @@ class Unicorn_Hat_HD
 {
   public:
     Unicorn_Hat_HD(uint8_t slaveSelectPin);
-	  void begin(void);
+	void begin(void);
     void show(void);
-	  void clear();
+	void clear();
     void setPixelColor(uint8_t n, uint8_t r, uint8_t g, uint8_t b);
 	void setPixelColor(uint8_t n, uint32_t c);
+	void setBrightness(uint8_t);
+	uint32_t Color(uint8_t r, uint8_t g, uint8_t b);
   private:
     uint8_t _slaveSelectPin;
     uint8_t _buff[16][16][3];
+	uint8_t brightness;
 };
 #endif
